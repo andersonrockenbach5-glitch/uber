1567661c1c062c7c55c3de47c1dc1ae086b6b786
Anderson <anderson_rock98@hotmail.com>
2025-07-17 20:29:00 -0300

    Algumas melhorias nos bugs

--- firebase_init.js
+++ firebase_init.js
@@ -1,18 +1,27 @@
-// SEU OBJETO firebaseConfig AQUI!
-// Copie do Console Firebase (Configurações do Projeto > Seus apps > Configuração SDK)
+// firebase_init.js
+
+// Importações modulares do Firebase SDK
+import { initializeApp } from 'https://www.gstatic.com/firebasejs/9.6.10/firebase-app.js';
+import { getAuth } from 'https://www.gstatic.com/firebasejs/9.6.10/firebase-auth.js';
+import { getFirestore } from 'https://www.gstatic.com/firebasejs/9.6.10/firebase-firestore.js';
+
+// Sua configuração do Firebase
 const firebaseConfig = {
-  apiKey: "AIzaSyCee4pXFdNxmdSv4VA90KdihyapXvAGt8w",
-  authDomain: "controle-financeiro-e190f.firebaseapp.com",
-  projectId: "controle-financeiro-e190f",
-  storageBucket: "controle-financeiro-e190f.firebasestorage.app",
-  messagingSenderId: "711653253846",
-  appId: "1:711653253846:web:f633ce5cc025e2c8aedf77",
-  measurementId: "G-ZKEY3BJN9G"
+    apiKey: "AIzaSyCee4pXFdNxmdSv4VA90KdihyapXvAGt8w",
+    authDomain: "controle-financeiro-e190f.firebaseapp.com",
+    projectId: "controle-financeiro-e190f",
+    storageBucket: "controle-financeiro-e190f.firebasestorage.app",
+    messagingSenderId: "711653253846",
+    appId: "1:711653253846:web:f633ce5cc025e2c8aedf77",
+    measurementId: "G-ZKEY3BJN9G"
 };
 
-// Initialize Firebase
-firebase.initializeApp(firebaseConfig);
+// Inicializa o Firebase
+const app = initializeApp(firebaseConfig);
 
-// Exporte as instâncias para que possam ser usadas em outras páginas
-const auth = firebase.auth();
-const db = firebase.firestore();
+// Inicializa serviços Firebase e atribui-os a variáveis
+const auth = getAuth(app);
+const db = getFirestore(app);
+
+// EXPORTA os objetos para que possam ser importados em outros arquivos
+export { auth, db, firebaseConfig };
